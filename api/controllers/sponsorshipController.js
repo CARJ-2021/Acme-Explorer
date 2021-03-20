7e7d3e009ebfb21c0a189138b2f3718f86e682dc
Roberto Hermoso <rhermoso98@gmail.com>
2021-03-20 17:55:34 +0100

    CRUDS Sponsorhips with auth

--- api/controllers/sponsorshipController.js
+++ api/controllers/sponsorshipController.js
@@ -1,60 +1,173 @@
 "use strict";
 /*---------------SPONSORSHIP----------------------*/
 var mongoose = require("mongoose"),
-    Sponsorship = mongoose.model("Sponsorship");
+  Sponsorship = mongoose.model("Sponsorship");
+
+var authController = require("../controllers/authController");
 
 exports.list_all_sponsorships = function (req, res) {
-    Sponsorship.find({}, function (err, sponsorships) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(sponsorships);
-        }
-    });
+  Sponsorship.find({}, function (err, sponsorships) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(sponsorships);
+    }
+  });
 };
 
 exports.create_an_sponsorship = function (req, res) {
-    var new_sponsorship = new Sponsorship(req.body);
-    new_sponsorship.save(function (err, sponsorship) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(sponsorship);
-        }
-    });
+  var new_sponsorship = new Sponsorship(req.body);
+  new_sponsorship.save(function (err, sponsorship) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(sponsorship);
+    }
+  });
 };
 
 exports.read_an_sponsorship = function (req, res) {
-    Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(sponsorship);
-        }
-    });
+  Sponsorship.findById(req.params.sponsorshipId, function (err, sponsorship) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(sponsorship);
+    }
+  });
 };
 
 exports.update_an_sponsorship = function (req, res) {
-    Sponsorship.findOneAndUpdate(
+  Sponsorship.findOneAndUpdate(
+    { _id: req.params.sponsorshipId },
+    req.body,
+    { new: true },
+    function (err, sponsorship) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(sponsorship);
+      }
+    }
+  );
+};
+
+exports.delete_an_sponsorship = function (req, res) {
+  Sponsorship.deleteOne(
+    { _id: req.params.sponsorshipId },
+    function (err, sponsorship) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ message: "Sponsorship successfully deleted" });
+      }
+    }
+  );
+};
+
+exports.list_all_sponsorships_v2 = async function (req, res) {
+  var idToken = req.headers["idtoken"];
+  var authenticatedUserId = await authController.getUserId(idToken);
+  Sponsorship.find(
+    { sponsor: authenticatedUserId },
+    function (err, sponsorships) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(sponsorships);
+      }
+    }
+  );
+};
+
+exports.create_a_sponsorship_v2 = async function (req, res) {
+  var idToken = req.headers["idtoken"];
+  var authenticatedUserId = await authController.getUserId(idToken);
+  var new_sponsorship = new Sponsorship(req.body);
+  new_sponsorship.sponsor = authenticatedUserId;
+  new_sponsorship.paid = false;
+  new_sponsorship.save(function (err, sponsorship) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(sponsorship);
+    }
+  });
+};
+
+exports.read_a_sponsorship_v2 = async function (req, res) {
+  var idToken = req.headers["idtoken"];
+  var authenticatedUserId = await authController.getUserId(idToken);
+  Sponsorship.findById(
+    req.params.sponsorshipId,
+    async function (err, sponsorship) {
+      if (err) {
+        res.send(err);
+      } else if (
+        JSON.stringify(sponsorship.sponsor) !=
+        JSON.stringify(authenticatedUserId)
+      )
+        res
+          .status(403)
+          .send("You don't have permissions to read this sponsorship");
+      else {
+        res.json(sponsorship);
+      }
+    }
+  );
+};
+
+exports.update_a_sponsorship_v2 = async function (req, res) {
+  var idToken = req.headers["idtoken"];
+  var authenticatedUserId = await authController.getUserId(idToken);
+  Sponsorship.findById(req.params.sponsorshipId, function (error, sponshp) {
+    if (error) res.send(err);
+    else if (sponshp == null) res.status(404).send("Sponsorship not found");
+    else if (
+      JSON.stringify(sponshp.sponsor) != JSON.stringify(authenticatedUserId)
+    )
+      res
+        .status(401)
+        .send("Only the creator can update his or her sponsorship");
+    else {
+      Sponsorship.findOneAndUpdate(
         { _id: req.params.sponsorshipId },
         req.body,
         { new: true },
         function (err, sponsorship) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.json(sponsorship);
-            }
+          if (err) {
+            res.send(err);
+          } else {
+            res.json(sponsorship);
+          }
         }
-    );
+      );
+    }
+  });
 };
 
-exports.delete_an_sponsorship = function (req, res) {
-    Sponsorship.deleteOne({ _id: req.params.sponsorshipId }, function (err, sponsorship) {
-        if (err) {
+exports.delete_a_sponsorship_v2 = async function (req, res) {
+  var idToken = req.headers["idtoken"];
+  var authenticatedUserId = await authController.getUserId(idToken);
+  Sponsorship.findById(req.params.sponsorshipId, function (error, sponshp) {
+    if (error) res.send(err);
+    else if (sponshp == null) res.status(404).send("Sponsorship not found");
+    else if (
+      JSON.stringify(sponshp.sponsor) != JSON.stringify(authenticatedUserId)
+    )
+      res
+        .status(401)
+        .send("Only the creator can delete his or her sponsorship");
+    else {
+      Sponsorship.remove(
+        { _id: req.params.sponsorshipId },
+        function (err, sponsorship) {
+          if (err) {
             res.send(err);
-        } else {
+          } else {
             res.json({ message: "Sponsorship successfully deleted" });
+          }
         }
-    });
+      );
+    }
+  });
 };
--- api/routes/sponsorshipRoutes.js
+++ api/routes/sponsorshipRoutes.js
@@ -1,15 +1,53 @@
 "use strict";
 module.exports = function (app) {
-    var sponsorships = require("../controllers/sponsorshipController");
+  var sponsorships = require("../controllers/sponsorshipController");
+  var authController = require("../controllers/authController");
 
-    app
-        .route("/v1/sponsorships")
-        .get(sponsorships.list_all_sponsorships)
-        .post(sponsorships.create_an_sponsorship);
+  //------- V1 -------
 
-    app
-        .route("/v1/sponsorships/:sponsorshipId")
-        .get(sponsorships.read_an_sponsorship)
-        .put(sponsorships.update_an_sponsorship)
-        .delete(sponsorships.delete_an_sponsorship);
+  app
+    .route("/v1/sponsorships")
+    .get(sponsorships.list_all_sponsorships)
+    .post(sponsorships.create_an_sponsorship);
+
+  app
+    .route("/v1/sponsorships/:sponsorshipId")
+    .get(sponsorships.read_an_sponsorship)
+    .put(sponsorships.update_an_sponsorship)
+    .delete(sponsorships.delete_an_sponsorship);
+
+  //------- V2 -------
+
+  app
+    .route("/v2/sponsorships")
+    .get(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.list_all_sponsorships_v2
+    )
+    .post(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.create_a_sponsorship_v2
+    );
+
+  app
+    .route("/v2/sponsorships/:sponsorshipId")
+    .get(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.read_a_sponsorship_v2
+    )
+    .put(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.update_a_sponsorship_v2
+    )
+    .delete(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.delete_a_sponsorship_v2
+    );
+
+  app
+    .route("/api/v2/sponsorships/:sponsorshipId/pay")
+    .put(
+      authController.verifyUser(["SPONSOR"]),
+      sponsorships.pay_a_sponsorship
+    );
 };

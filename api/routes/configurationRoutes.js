26c87f8b48e1fcc3b0e4662c3c9b10ac429eca70
CesarGP <cgpcsjcmp@gmail.com>
2021-03-23 11:58:47 +0100

    feat: configuration for administrators

--- api/routes/configurationRoutes.js
+++ api/routes/configurationRoutes.js
@@ -1,10 +1,16 @@
 "use strict";
 module.exports = function (app) {
-    var configuration = require("../controllers/configurationController");
+    var configuration = require("../controllers/configurationController"),
+        authController = require("../controllers/authController");
 
 
     app
         .route("/v1/configuration")
         .get(configuration.read_configuration)
         .put(configuration.update_configuration);
+
+    app
+        .route("/v2/configuration")
+        .get(authController.verifyUser(["ADMINISTRATOR"]), configuration.read_configuration)
+        .put(authController.verifyUser(["ADMINISTRATOR"]), configuration.update_configuration);
 };
